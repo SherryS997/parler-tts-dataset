b1f2997ab3942ab7c45b804ccab324ed493360c8
Sherry Thomas <sherrythomas32@protonmail.com>
2024-10-02 11:41:04 +0530

    Ported to Vue

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
 
 module.exports = defineConfig({
-  outputDir: path.resolve(__dirname, './docs'),
+  outputDir: path.resolve(__dirname, './docs'), // Set output directory for the build
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? '/parler-tts-dataset/' // Base path for GitHub Pages
+    : '/', // Base path for local development
   transpileDependencies: true
-})
+});
